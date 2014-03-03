b2d6064851745b3b9bf8a35d98f12fc17f03bed9
Luigi Pirelli <luipir@gmail.com>
2014-03-03 16:43:51 +0100

    bug in regexp to allow parsing also in linux where not additional chars ar added to the end of line

--- mapfileexportdlg.py
+++ mapfileexportdlg.py
@@ -469,7 +469,7 @@ class MapfileExportDlg(QDialog, Ui_MapfileExportDlg):
         # add the FONTSET keyword with the associated path
         if self.txtMapFontsetPath.text() != "":
             # get the index of the first instance of MAP string in the list
-            pos = parts.index( filter(lambda x: re.compile("^MAP(\r\n|\r|\n)$").match(x), parts)[0] )
+            pos = parts.index( filter(lambda x: re.compile("^MAP(\r\n|\r|\n)*$").match(x), parts)[0] )
             if pos >= 0:
                 parts.insert( pos+1, u'  FONTSET "%s"' % self.txtMapFontsetPath.text() )
                 partsContentChanged = True
